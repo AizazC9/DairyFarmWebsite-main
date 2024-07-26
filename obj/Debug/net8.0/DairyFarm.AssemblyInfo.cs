039298cadca744828adece2aacfa7a3a4874646f
AizazC9 <bsef20m552@pucit.edu.pk>
2024-07-26 12:40:40 +0500

    alert issue reslove

--- obj/Debug/net8.0/DairyFarm.AssemblyInfo.cs
+++ obj/Debug/net8.0/DairyFarm.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DairyFarm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18b71bf7c7ace94b382a670a4a88df4c3b74905e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DairyFarm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DairyFarm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
